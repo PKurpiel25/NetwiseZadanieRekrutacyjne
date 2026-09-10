37c8ec1c55f5de48a9c92d11409c0cefcb5bc0f2
PKurpiel25 <pkurpiel25@gmail.com>
2026-09-10 14:36:26 +0200

    Implement core functionality.

--- FunCatFacts/Program.cs
+++ FunCatFacts/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using FunCatFacts.DTO;
+using FunCatFacts.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+ServiceCollection services = new();
+
+services.AddHttpClient<APIAccessService>("FunCatFacts");
+
+var provider = services.BuildServiceProvider();
+
+var apiAccess = provider.GetRequiredService<APIAccessService>();
+
+FunFact? funFact = await apiAccess.GetFunFactAsync();
+
+if(funFact != null)
+{
+    Console.WriteLine(funFact);
+
+    await FileWriterService.WriteTextToFileAsync(funFact.ToString());
+}
+else
+{
+    Console.WriteLine("No fun fact to show!");
+}
+
+Console.WriteLine("Press any key to close.");
+    
+Console.ReadKey();
